cd3268d4359e84312efddfe055630913158f5cbe
Fernando Paniagua <fernando.paniagua.fsg.2021@gmail.com>
2022-04-08 11:16:50 +0200

    Initial commit

---
+++ src/main/java/com/fernandopaniagua/vehiculosmiticos/VehiculosmiticosApplication.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.vehiculosmiticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosmiticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosmiticosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandopaniagua/vehiculosmiticos/VehiculosmiticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.vehiculosmiticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosmiticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
